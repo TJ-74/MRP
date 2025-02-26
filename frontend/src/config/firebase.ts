92d4a8982369196f27764e198dbf69c9d5abecad
TJ-74 <tarunsai.janapati@slu.edu>
2025-02-26 15:56:16 -0600

    Update firebase.ts

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -5,13 +5,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDvmUub6rVp-n5g3jEZJenkVUXc2x6SZk0",
-  authDomain: "boronatom-ai.firebaseapp.com",
-  projectId: "boronatom-ai",
-  storageBucket: "boronatom-ai.firebasestorage.app",
-  messagingSenderId: "347981786413",
-  appId: "1:347981786413:web:505266d882b01c1cb5f7a4",
-  measurementId: "G-80GPT0N0QL"
+
 };
 
 
